23d79481b4c17ac3d2c714db94c7e8aec11a63bc
Marawanelbalal <marawananbu100@gmail.com>
2026-03-05 15:00:00 +0200

    Added data creation script and removed db file

---
+++ backend/app/data_creation.py
@@ -0,0 +1,11 @@
+import sqlite3
+import os
+import pandas as pd
+from datasets import load_dataset
+hadith_data = load_dataset("meeAtif/hadith_datasets", split='train')
+df = pd.DataFrame(hadith_data)
+DB_DIR = os.path.join(os.path.dirname(__file__),'..','data','hadiths.db')
+connection = sqlite3.connect(DB_DIR)
+df.to_sql("HADITHS",connection,if_exists="replace", index = True, index_label = 'id')
+print(pd.read_sql("SELECT COUNT(*) FROM HADITHS", connection))
+connection.close()
